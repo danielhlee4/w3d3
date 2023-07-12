43cc96dd8f6598102cbe246eb4de2ea2154ff1ed
Dan Lee <danielhlee4@gmail.com>
2023-07-12 14:40:40 -0400

    duped the dup

--- recurssion/problems.rb
+++ recurssion/problems.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 def iter_range(start, end_ele)
     if end_ele < start
         return []
@@ -24,17 +26,17 @@ end
 # p rec_range(1, 3) # =>  [1, 2]
 # p rec_range(1, 4) # =>  [1, 2, 3]
 
-# def exp(b,n)
-#     #base case
-#     return 0 if b==0 && n==0
-#     return 1 if n ==0
-#     return b if n ==1 
-
-#     #inductive step
-#     exp(b,n-1) *b
-# end
-
 def exp(b,n)
+    #base case
+    return 0 if b==0 && n==0
+    return 1 if n ==0
+    return b if n ==1 
+
+    #inductive step
+    exp(b,n-1) *b
+end
+
+def exp_better(b,n)
     #base case
     return 0 if b==0 && n==0
     return 1 if n ==0
@@ -45,12 +47,50 @@ def exp(b,n)
     (exp(b, (n - 1) / 2) ** 2) *b if n.odd?
 end
 
-p exp(0,0) #=> 0
-p exp(0,1) #=> 0
-p exp(1,0) #=> 1
-p exp(1,1) #=> 1
-p exp(1,2) #=> 1 => exp(1,1) * 1
-p exp(2,0) #=> 1 =>
-p exp(2,1) #=> 2
-p exp(2,2) #=> 4 => exp(2,1) * 2
-p exp(2,8)
+# p exp(0,0) #=> 0
+# p exp(0,1) #=> 0
+# p exp(1,0) #=> 1
+# p exp(1,1) #=> 1
+# p exp(1,2) #=> 1 => exp(1,1) * 1
+# p exp(2,0) #=> 1 =>
+# p exp(2,1) #=> 2
+# p exp(2,2) #=> 4 => exp(2,1) * 2
+# p exp(2,8)
+class Array
+
+    def dup
+        #base case
+        return self if !self.is_a?(Array) 
+        # below this line, self is an array
+        dup_arr = []
+        self.each do |ele|
+            dup_arr << ele.dup
+        end
+
+        dup_arr
+    end
+end
+
+# a = [1, 2, [2, 3] ]
+# b = a.dup
+# b[2] << 4
+# p a # [1, 2, [2, 3] ]
+# p b # [1, 2, [2, 3, 4] ]
+
+# p a.object_id
+# p b.object_id
+# p a[2].object_id
+# p b[2].object_id
+
+# robot_parts = [
+#   ["nuts", "bolts", "washers"],
+#   ["capacitors", "resistors", "inductors"]
+# ]
+
+# robot_parts_copy = robot_parts.dup
+
+# robot_parts_copy[1] << "LEDs"
+
+# p robot_parts[1] # => ["capacitors", "resistors", "inductors"]
+
+
